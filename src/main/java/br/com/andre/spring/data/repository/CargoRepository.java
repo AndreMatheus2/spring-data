fd4d9dea91854ab7ce7b51d36bb4b506e5f8f00b
andrematheus2 <am_arao@hotmail.com>
2022-08-31 10:08:51 -0300

    inserindo valores com CrudRepository

--- src/main/java/br/com/andre/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/andre/spring/data/repository/CargoRepository.java
@@ -1,10 +1,11 @@
 package br.com.andre.spring.data.repository;
 
+import br.com.andre.spring.data.orm.Cargo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CargoRepository extends CrudRepository {
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
 
 
 
