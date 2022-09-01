c89de8ad28cf63fb69d798fe345e0905cfd312c2
andrematheus2 <am_arao@hotmail.com>
2022-09-01 09:26:24 -0300

    interface de projeção e nativeQuery

--- src/main/java/br/com/andre/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/andre/spring/data/repository/FuncionarioRepository.java
@@ -1,14 +1,15 @@
 package br.com.andre.spring.data.repository;
 
+import br.com.andre.spring.data.orm.FuncionarioProjecao;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.andre.spring.data.orm.Funcionario;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.List;
 
 @Repository
@@ -17,4 +18,7 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 
     @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
     List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+
+    @Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
+    List<FuncionarioProjecao> findFuncionarioSalario();
 }
